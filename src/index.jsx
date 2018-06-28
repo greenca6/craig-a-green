b370b8bebf9bdebbfff572539c6ccb84958026c2
craig-green <CGreen@sbd2.com>
2018-06-28 13:44:05 -0400

    Remove files that should be ignored. Add bootstrap and reactstrap

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './favicon.ico';
+
 ReactDOM.render(<App />, document.getElementById('app'));
